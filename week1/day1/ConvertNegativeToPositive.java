83b322bb351bb540288e819341e0243f9ef30d19
SwapnaRajasekaran <110966640+SwapnaRajasekaran@users.noreply.github.com>
2022-08-11 16:37:18 +0530

    Week1.day1

---
+++ week1/day1/ConvertNegativeToPositive.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class ConvertNegativeToPositive {
+	public static void main(String[] args) {
+		int a=-14;
+		int b;
+		if (a<0) {
+			b=(a*(-1));
+			System.out.println("The given number is converted to positive" +b);
+			}
+		else
+		{
+			
+		System.out.println("The number is negative");
+						
+	}}
+
+	
+		// TODO Auto-generated method stub
+
+	}
+
+
---
+++ week1/day1/PositiveOrNegative.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class PositiveOrNegative {
+  public static void main(String[] args) 
+  {
+	  int i=11;
+	  if (i>0)
+		  System.out.println("The Number is positive");
+	  else
+		  System.out.println("The Number is negative");
+	
+}
+	
+}
+
+
